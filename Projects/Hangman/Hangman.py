d8d0b4b367dda4be74a467833edf5064847c2b59
CSI-Carlos-Xu <24-099@sanignacio.pr>
2022-02-10 10:42:25 -0400

    Hangman

---
+++ Projects/Hangman/Hangman.py
@@ -0,0 +1,58 @@
+import random 
+word_list = ["gimnasia", "tenis", "ciclismo", "rugby", "boliche", "vela", "baloncesto", "voleibol", "remo", "surfing", "patinaje", "balompie", "natacion", "pelota", "ajedrez", "golf", "badminton", "boxeo", "esgrima", "hockey"]
+
+steps = [""" 
+
+--------
+|
+|
+|
+|
+|
+>>>>>>>>>>>>>>>>
+
+--------
+|     |
+|
+|
+|
+|
+>>>>>>>>>>>>>>>>
+
+--------
+|     |
+|     O
+|
+|
+|
+>>>>>>>>>>>>>>>>
+
+--------
+|     |
+|     O
+|     |
+|
+|
+>>>>>>>>>>>>>>>>
+
+--------
+|     |
+|     O
+|    -|-
+|
+|
+>>>>>>>>>>>>>>>>
+
+--------
+|     |
+|     O
+|    -|-
+|    ? ?
+|
+>>>>>>>>>>>>>>>>
+
+
+"""]
+        
+
+print(steps[0])
