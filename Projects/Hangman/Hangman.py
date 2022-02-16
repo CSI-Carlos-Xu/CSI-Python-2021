e8c2f24f45f784c1bac1c90d0e61b0cb509fe2f6
CSI-Carlos-Xu <24-099@sanignacio.pr>
2022-02-16 10:42:51 -0400

    Hangman

--- Projects/Hangman/Hangman.py
+++ Projects/Hangman/Hangman.py
@@ -1,3 +1,4 @@
+from imp import is_frozen
 import random 
 word_list = ["gimnasia", "tenis", "ciclismo", "rugby", "boliche", "vela", "baloncesto", "voleibol", "remo", "surfing", "patinaje", "balompie", "natacion", "pelota", "ajedrez", "golf", "badminton", "boxeo", "esgrima", "hockey"]
 
@@ -56,3 +57,34 @@ steps = ["""
         
 
 print(steps[0])
+
+def get_word():
+    word_list =  ["gimnasia", "tenis", "ciclismo", "rugby", "boliche", "vela", "baloncesto", "voleibol", "remo", "surfing", "patinaje", "balompie", "natacion", "pelota", "ajedrez", "golf", "badminton", "boxeo", "esgrima", "hockey"]
+    return random.choice(word_list)
+
+def find_input():
+
+    letter = input("choose letter")
+
+    # if(letter != #)
+
+    while not guessed and tries > 0:
+        guess = input("Guess the word:").upper()
+        if len(guess) ==1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("already used that letter", guess)
+                elif guess not in word:
+                    print(guess,"Not the word:")
+
+
+
+
+    # if(letter.isalpha()):
+    #     print("the given character does not contain special characters")   
+
+
+    # elif(letter.isalpha):
+    #     print("Invalid")
+
+
+
