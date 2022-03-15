941a9d7d9ac0cb2418802e47008506954065a123
CSI-Carlos-Xu <24-099@sanignacio.pr>
2022-03-15 10:12:25 -0400

    Snake- Block 1

---
+++ Projects/Snake/Snake.py
@@ -0,0 +1,6 @@
+import pygame #It calls the module where the game is.
+pygame.init() #This step initialize the game.
+dis=pygame.display.set_mode((400,300)) #This creates a display for the game. 
+pygame.display.update() #This step updates the display of the screen. 
+pygame.quit() #uninitialize the screen display
+quit() #uninitialize everything
